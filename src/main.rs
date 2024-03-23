2e79dbfc96ddc50c7cb1fd776558b0146eb8eb2f
jiisanda <jiisanda@protonmail.com>
2024-03-23 20:00:15 +0530

    web setup - cors, db-url, routes

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,16 @@
+mod handler;
 mod model;
 mod schema;
 
+use actix_cors::Cors;
 use actix_web::middleware::Logger;
-use actix_web::{App, get, HttpResponse, HttpServer, Responder};
-use serde_json::json;
+use actix_web::{http::header, web, App, HttpServer};
+use dotenv::dotenv;
+use sqlx::postgres::PgPoolOptions;
+use sqlx::{Pool, Postgres};
 
-
-#[get("/api/health-checker")]
-async fn health_checker_handler() -> impl Responder {
-    const MESSAGE: &str = "Building simple CRUD API, using Rust, SQLX, Postgres and Actix-Web";
-
-    HttpResponse::Ok().json(json!({
-        "status": "success",
-        "message": MESSAGE
-    }))
+pub struct AppState {
+    db: Pool<Postgres>,
 }
 
 #[actix_web::main]
@@ -21,17 +18,44 @@ async fn main() -> std::io::Result<()> {
     if std::env::var_os("RUST_LOG").is_none() {
         std::env::set_var("RUST_LOG", "actix_web=info");
     }
-
+    dotenv().ok();
     env_logger::init();
+
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = match PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&database_url)
+        .await
+    {
+        Ok(pool) => {
+            println!("✔️ Connection to the database is successful!");
+            pool
+        }
+        Err(err) => {
+            println!("🤔 Fail to connect to the database: {:?}", err);
+            std::process::exit(1);
+        }
+    };
 
     println!("🦀 Server started successfully...");
 
     HttpServer::new(move || {
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:3000")
+            .allowed_methods(vec!["GET", "POST", "PATCH", "DELETE"])
+            .allowed_headers(vec![
+                header::CONTENT_TYPE,
+                header::AUTHORIZATION,
+                header::ACCEPT,
+            ])
+            .supports_credentials();
         App::new()
-            .service(health_checker_handler)
+            .app_data(web::Data::new(AppState { db: pool.clone() }))
+            .configure(handler::config)
+            .wrap(cors)
             .wrap(Logger::default())
     })
-        .bind(("127.0.0.1", 8000))?
-        .run()
-        .await
+    .bind(("127.0.0.1", 8000))?
+    .run()
+    .await
 }
